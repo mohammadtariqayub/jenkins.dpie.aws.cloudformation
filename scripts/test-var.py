a82e1c81e87be243d1131d661d9dc65ef8cece4b
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-10 09:47:50 +1000

    adding test file

---
+++ scripts/test-var.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import json
+import uuid
+import boto3
+from time import time
+
+### Global variables ###
+account_id = os.environ['AWSAccountNumber']
+ImageId = os.environ['ImageId']
+HostName = os.environ['HostName']
+SubnetId = os.environ['SubnetId']
+SecurityGroupIds = os.environ['SecurityGroupIds']
+SystemOwner = os.environ['SystemOwner']
+AWS_REGION = os.environ['Region']
+
+echo (account_id)
+echo (SystemOwner)
