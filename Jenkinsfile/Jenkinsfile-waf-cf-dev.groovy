900cb711d846db0bb1ea6f38e3fe2e946cd0ed27
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-10 16:27:56 +1000

    updating Jenkinsfile-waf-cf-dev.groovy

--- Jenkinsfile/Jenkinsfile-waf-cf-dev.groovy
+++ Jenkinsfile/Jenkinsfile-waf-cf-dev.groovy
@@ -4,7 +4,13 @@ node ('master') {
     echo "deploy ec2"
     build job: 'ec2-single',
            parameters: [
-                string(name: 'HostName', value: params['HostName'])
+                string(name: 'AWSAccountNumber', value: params['AWSAccountNumber']),
+                string(name: 'ImageId', value: params['ImageId']),
+                string(name: 'HostName', value: params['HostName']),
+                string(name: 'SubnetId', value: params['SubnetId']),
+                string(name: 'SecurityGroupIds', value: params['SecurityGroupIds']),
+                string(name: 'SystemOwner', value: params['SystemOwner']),
+                string(name: 'Region', value: params['Region'])
            ]
   }
 }
