551742647c2c882c8c0f02255a6a546ad6a4dd60
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-11 15:59:26 +1000

    adding scripts/create_acm_syd.py

--- Jenkinsfile/Jenkinsfile-temp.groovy
+++ Jenkinsfile/Jenkinsfile-temp.groovy
@@ -4,23 +4,20 @@ node ('master') {
     git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
   }
   
-    stage('create acm certificate us1') {
-    echo "creating acm certificate us1"
-    build job: 'acm-cert-us1',
+  stage('deploy security group') {
+    echo "deploying security group"
+    build job: 'cloudfront-sg',
            parameters: [
                 string(name: 'StackName', value: params['StackName']),
                 string(name: 'AWSAccountNumber', value: params['AWSAccountNumber']),
-                string(name: 'ApexDomainName', value: params['ApexDomainName']),
+                string(name: 'Region', value: params['Region']),
                 string(name: 'Application', value: params['Application']),
-                string(name: 'BusinessOwner', value: params['BusinessOwner']),
-                string(name: 'BusinessUnit', value: params['BusinessUnit']),
+                string(name: 'AWSAccountSSMParameter', value: params['AWSAccountSSMParameter']),
                 string(name: 'Comments', value: params['Comments']),
-                string(name: 'DomainName', value: params['DomainName']),
-                string(name: 'Environment', value: params['Environment']),
+                string(name: 'DeploymentBucket', value: params['DeploymentBucket']),
                 string(name: 'ProjectCode', value: params['ProjectCode']),
-                string(name: 'RecID', value: params['RecID']),
-                string(name: 'RFC', value: params['RFC']),
-                string(name: 'AWSAccount', value: params['AWSAccount']),
+                string(name: 'Environment', value: params['Environment']),
+                string(name: 'VpcId', value: params['VpcId']),
                 string(name: 'SystemOwner', value: params['SystemOwner'])
            ]
   }
