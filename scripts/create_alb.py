e9c097301592a94ca21cd4931d27c24a886cc684
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-17 16:35:53 +1000

    adding temp folder

--- scripts/create_alb.py
+++ scripts/create_alb.py
@@ -41,11 +41,11 @@ sg2_artifact = open(path,'r')
 sg2_ID = sg2_artifact.read()
 
 ### Local variables
-ALBName = Application + "-" + "Environment" + "-" + "ext-alb"
+ALBName = Application + "-" + Environment + "-" + "ext-alb"
 CFNTemplate = 'create-alb.yml'
 CertificateArn = 'Acm_syd_ARN'
-CloudFrontGlobalSecurityGroupID = 'sg1_ID'
-CloudFrontRegionalSecurityGroupID = 'sg2_ID'
+CloudFrontGlobalSecurityGroupID = sg1_ID
+CloudFrontRegionalSecurityGroupID = sg2_ID
 SslPolicy = 'ELBSecurityPolicy-TLS-1-2-2017-01'
 Role = 'web'
 
