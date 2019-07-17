d2aee899175c6e04d464d3103eb8165877728410
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-17 14:37:26 +1000

    adding sns subscription

--- scripts/create_sns_sub_us1.py
+++ scripts/create_sns_sub_us1.py
@@ -116,7 +116,7 @@ def main():
     stack_id = int(time())
     jenkins_job_name = os.environ['StackName']
     jenkins_build_number = os.environ['BUILD_NUMBER']
-    stack_name = jenkins_job_name + "-" + "acm-us1"
+    stack_name = jenkins_job_name + "-" + "sns-us1"
     print ("Using stack name: " + stack_name)
 
     cft_params = []
