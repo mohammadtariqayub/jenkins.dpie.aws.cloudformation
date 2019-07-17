c7b30111a5fdf5b091672bea357f43088d8b0380
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-17 20:21:00 +1000

    adding temp folder

--- Jenkinsfile/Jenkinsfile-waf-cf-dev.groovy
+++ Jenkinsfile/Jenkinsfile-waf-cf-dev.groovy
@@ -74,4 +74,29 @@ node ('master') {
            ]
   }
 
+  stage('deploy alb') {
+    echo "deploying alb"
+    build job: 'alb-CD',
+           parameters: [
+                string(name: 'StackName', value: params['StackName']),
+                string(name: 'AWSAccountNumber', value: params['AWSAccountNumber']),
+                string(name: 'Region', value: params['Region']),
+                string(name: 'Application', value: params['Application']),
+                string(name: 'ALBScheme', value: params['ALBScheme']),
+                string(name: 'ALBAcessLogBucket', value: params['ALBAcessLogBucket']),
+                string(name: 'VpcId', value: params['VpcId']),
+                string(name: 'PublicSubnetA', value: params['PublicSubnetA']),
+                string(name: 'PublicSubnetB', value: params['PublicSubnetB']),
+                string(name: 'PublicSubnetC', value: params['PublicSubnetC']),
+                string(name: 'EC2Instance1', value: params['EC2Instance1']),
+                string(name: 'EC2Instance2', value: params['EC2Instance2']),
+                string(name: 'BusinessOwner', value: params['BusinessOwner']),
+                string(name: 'BusinessUnit', value: params['BusinessUnit']),
+                string(name: 'Criticality', value: params['Criticality']),
+                string(name: 'Environment', value: params['Environment']),
+                string(name: 'RecID', value: params['RecID']),
+                string(name: 'SystemOwner', value: params['SystemOwner'])
+           ]
+  }
+
 }
