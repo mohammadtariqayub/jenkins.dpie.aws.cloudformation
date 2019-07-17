cfda37362279da2c83fcd9c4aead2b90eb34288b
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-17 14:54:47 +1000

    adding sns subscription

---
+++ scripts/invloking-lambda.sh
@@ -0,0 +1,26 @@
+
+echo "invoking lambda function"
+lambda_function="`cat /root/artifacts/lambda-function-$StackName`"
+
+
+/root/.local/lib/aws/bin/aws lambda invoke --invocation-type RequestResponse --function-name vdcportal-cloudfront-iprange-autoupdate-function --region ap-southeast-2 --log-type Tail --payload '{
+  "Records": [
+    {
+      "EventVersion": "1.0",
+      "EventSubscriptionArn": "arn:aws:sns:EXAMPLE",
+      "EventSource": "aws:sns",
+      "Sns": {
+        "SignatureVersion": "1",
+        "Timestamp": "1970-01-01T00:00:00.000Z",
+        "Signature": "EXAMPLE",
+        "SigningCertUrl": "EXAMPLE",
+        "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
+        "Message": "{\"create-time\": \"yyyy-mm-ddThh:mm:ss+00:00\", \"synctoken\": \"0123456789\", \"md5\": \"436ae5a9ba06e4c4511cb0a7c1ef445b\", \"url\": \"https://ip-ranges.amazonaws.com/ip-ranges.json\"}",
+        "Type": "Notification",
+        "UnsubscribeUrl": "EXAMPLE",
+        "TopicArn": "arn:aws:sns:EXAMPLE",
+        "Subject": "TestInvoke"
+      }
+    }
+  ]
+}' /tmp/outputfile.txt
