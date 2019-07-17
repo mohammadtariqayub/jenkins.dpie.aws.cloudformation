27f2dfe01368e690e68d0630594de6d74115164e
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-17 14:33:51 +1000

    adding sns subscription

--- Jenkinsfile/Jenkinsfile-temp.groovy
+++ Jenkinsfile/Jenkinsfile-temp.groovy
@@ -4,21 +4,12 @@ node ('master') {
     git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
   }
   
-  stage('deploy security group') {
-    echo "deploying security group"
-    build job: 'cloudfront-sg',
+  stage('subcsribe SNS topic for lambda') {
+    echo "subcsribe SNS topic for lambda"
+    build job: 'sns-us1',
            parameters: [
                 string(name: 'StackName', value: params['StackName']),
-                string(name: 'AWSAccountNumber', value: params['AWSAccountNumber']),
-                string(name: 'Region', value: params['Region']),
-                string(name: 'Application', value: params['Application']),
-                string(name: 'AWSAccountSSMParameter', value: params['AWSAccountSSMParameter']),
-                string(name: 'Comments', value: params['Comments']),
-                string(name: 'DeploymentBucket', value: params['DeploymentBucket']),
-                string(name: 'ProjectCode', value: params['ProjectCode']),
-                string(name: 'Environment', value: params['Environment']),
-                string(name: 'VpcId', value: params['VpcId']),
-                string(name: 'SystemOwner', value: params['SystemOwner'])
+                string(name: 'AWSAccountNumber', value: params['AWSAccountNumber'])
            ]
   }
 
--- Jenkinsfile/Jenkinsfile-waf-cf-dev.groovy
+++ Jenkinsfile/Jenkinsfile-waf-cf-dev.groovy
@@ -47,4 +47,22 @@ node ('master') {
            ]
   }
 
+    stage('deploy security group') {
+    echo "deploying security group"
+    build job: 'cloudfront-sg',
+           parameters: [
+                string(name: 'StackName', value: params['StackName']),
+                string(name: 'AWSAccountNumber', value: params['AWSAccountNumber']),
+                string(name: 'Region', value: params['Region']),
+                string(name: 'Application', value: params['Application']),
+                string(name: 'AWSAccountSSMParameter', value: params['AWSAccountSSMParameter']),
+                string(name: 'Comments', value: params['Comments']),
+                string(name: 'DeploymentBucket', value: params['DeploymentBucket']),
+                string(name: 'ProjectCode', value: params['ProjectCode']),
+                string(name: 'Environment', value: params['Environment']),
+                string(name: 'VpcId', value: params['VpcId']),
+                string(name: 'SystemOwner', value: params['SystemOwner'])
+           ]
+  }
+
 }
