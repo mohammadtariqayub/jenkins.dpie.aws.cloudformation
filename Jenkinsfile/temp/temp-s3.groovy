342f598b55bdbc8b9dbf9948e62499dc9729be64
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-17 21:31:39 +1000

    adding temp folder

--- Jenkinsfile/temp/temp-s3.groovy
+++ Jenkinsfile/temp/temp-s3.groovy
@@ -4,27 +4,18 @@ node ('master') {
     git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
   }
   
-  stage('deploy s3') {
-    echo "deploying s3"
-    build job: 's3-CD',
+  stage('deploy s3 buckets') {
+    echo "deploy s3 buckets for WAF and CF"
+    build job: 's3-bucket-CD',
            parameters: [
                 string(name: 'StackName', value: params['StackName']),
-                string(name: 'AWSAccountNumber', value: params['AWSAccountNumber']),
-                string(name: 'Region', value: params['Region']),
                 string(name: 'Application', value: params['Application']),
-                string(name: 'ALBScheme', value: params['ALBScheme']),
-                string(name: 'ALBAcessLogBucket', value: params['ALBAcessLogBucket']),
-                string(name: 'VpcId', value: params['VpcId']),
-                string(name: 'PublicSubnetA', value: params['PublicSubnetA']),
-                string(name: 'PublicSubnetB', value: params['PublicSubnetB']),
-                string(name: 'PublicSubnetC', value: params['PublicSubnetC']),
-                string(name: 'EC2Instance1', value: params['EC2Instance1']),
-                string(name: 'EC2Instance2', value: params['EC2Instance2']),
+                string(name: 'EnvironmentType', value: params['EnvironmentType']),
                 string(name: 'BusinessOwner', value: params['BusinessOwner']),
                 string(name: 'BusinessUnit', value: params['BusinessUnit']),
-                string(name: 'Criticality', value: params['Criticality']),
                 string(name: 'Environment', value: params['Environment']),
-                string(name: 'RecID', value: params['RecID']),
+                string(name: 'ProjectCode', value: params['ProjectCode']),
+                string(name: 'RFC', value: params['RFC']),
                 string(name: 'SystemOwner', value: params['SystemOwner'])
            ]
   }
