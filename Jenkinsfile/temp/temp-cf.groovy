749f0900fd1e88a9d818a0990170fde0f9861e08
Mohammad Tariq Ayub <mohammad.tariq.ayub@outlook.com>
2019-07-18 16:35:00 +1000

    adding cf templete

---
+++ Jenkinsfile/temp/temp-cf.groovy
@@ -0,0 +1,24 @@
+node ('master') {
+ 
+  stage('template copy'){
+    git url: 'https://github.com/mohammadtariqayub/jenkins.aws.cloudformation.git'
+  }
+  
+  stage('deploy CloudFront') {
+    echo "deploy CF"
+    build job: 'cf-CD',
+           parameters: [
+                string(name: 'StackName', value: params['StackName']),
+                string(name: 'Application', value: params['Application']),
+                string(name: 'Region', value: params['Region']),
+                string(name: 'DomainName', value: params['DomainName']),
+                string(name: 'BusinessUnit', value: params['BusinessUnit']),
+                string(name: 'Criticality', value: params['Criticality']),
+                string(name: 'Environment', value: params['Environment']),
+                string(name: 'RecID', value: params['RecID']),
+                string(name: 'SystemOwner', value: params['SystemOwner']),
+                string(name: 'BusinessOwner', value: params['BusinessOwner'])
+           ]
+  }
+
+}
